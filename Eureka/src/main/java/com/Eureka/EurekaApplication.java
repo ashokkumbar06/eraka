ad2dca8e08457fa4f79e707845bd64b604a9011a
Ashok <ashokakumbar06@gmail.com>
2025-03-01 00:02:43 +0530

    nothing

--- Eureka/src/main/java/com/Eureka/EurekaApplication.java
+++ Eureka/src/main/java/com/Eureka/EurekaApplication.java
@@ -8,8 +8,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+        System.out.println("    A        SSSSS      H       H      OOOO      K      K  ");
+        System.out.println("   A A      S     S     H       H     O    O     K    K    ");
+        System.out.println("  AAAAA     S     s     H H H H H     O    O     K  K      ");
+        System.out.println(" A     A    S     S     H       H     O    O     K    K    ");
+        System.out.println("A       A    SSSSS      H       H      OOOO      K      K  ");
+    }
 }
